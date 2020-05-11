4589b40f97b54986acdc2ac357fc8783121f87b6
tinfoil-knight <kunal99kundu@gmail.com>
2020-05-11 21:34:11 +0530

    Added Logging

--- contact.go
+++ contact.go
@@ -1,9 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 )
+
+// Contact : Struct for Storing Contacts
+type Contact struct {
+	Name  string
+	Phone string
+}
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
@@ -19,7 +26,16 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 
 }
 
+func logRequest(handler http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s\n", r.Method, r.URL)
+		handler.ServeHTTP(w, r)
+	})
+}
+
 func main() {
+	httpPort := 8080
 	http.HandleFunc("/api", apiHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	fmt.Println("Server starting on http://localhost:8080")
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", httpPort), logRequest(http.DefaultServeMux)))
 }
