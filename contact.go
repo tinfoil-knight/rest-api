2899ce02a4e763a94eddbcf00c932525222304a8
tinfoil-knight <kunal99kundu@gmail.com>
2020-06-27 16:13:52 +0530

    DELETE Endpoint Works Now, Fixed Other Endpoints

--- contact.go
+++ contact.go
@@ -20,18 +20,20 @@ var client *mongo.Client
 
 // Contact : Struct for Storing Contacts
 type Contact struct {
-	// ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
-	Name  string `json:"name" bson:"name,omitempty"`
-	Phone string `json:"phone,omitempty" bson:"phone,omitempty"`
+	ID    primitive.ObjectID `json:"id,omitempty" bson:"_id,omitempty"`
+	Name  string             `json:"name" bson:"name,omitempty"`
+	Phone string             `json:"phone,omitempty" bson:"phone,omitempty"`
 }
 
+// TODO: Change BSON.D to BSON.M as sorting is not required
 func apiHandler(w http.ResponseWriter, r *http.Request) {
-
+	param := r.URL.Path[len("/api/"):]
+	fmt.Println(param)
 	w.Header().Set("Content-Type", "application/json")
 	collection := client.Database(os.Getenv("DB")).Collection(os.Getenv("COLLECTION"))
 	switch r.Method {
 	case "POST":
-		var contact *Contact
+		var contact Contact
 		json.NewDecoder(r.Body).Decode(&contact)
 		result, err := collection.InsertOne(context.TODO(), contact)
 		if err != nil {
@@ -40,44 +42,75 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		json.NewEncoder(w).Encode(result)
 	case "PUT":
-		var contact *Contact
+		id, _ := primitive.ObjectIDFromHex(param)
+		filter := bson.M{"_id": id}
+
+		var contact Contact
 		json.NewDecoder(r.Body).Decode(&contact)
-		filter := bson.D{primitive.E{Key: "name", Value: contact.Name}}
+
 		update := bson.D{primitive.E{Key: "$set", Value: bson.D{
 			primitive.E{Key: "phone", Value: contact.Phone},
 		}},
 		}
+
 		updateResult, err := collection.UpdateOne(context.TODO(), filter, update)
 		if err != nil {
 			sendErr(w, err, "Changing the record failed. Try again.")
 			return
 		}
 		json.NewEncoder(w).Encode(updateResult)
-
 	case "DELETE":
-		// TODO
-	case "GET":
-		filter := bson.D{{}}
-		var contacts []*Contact
-		cur, err := collection.Find(context.TODO(), filter)
+		id, _ := primitive.ObjectIDFromHex(param)
+		filter := bson.M{"_id": id}
+
+		deleteResult, err := collection.DeleteOne(context.TODO(), filter)
 		if err != nil {
-			sendErr(w, err, "Couldn't fetch the records. Try again.")
+			sendErr(w, err, "Couldn't delete the record. Try again.")
 			return
 		}
-		defer cur.Close(context.TODO())
-		for cur.Next(context.TODO()) {
-			var elem Contact
-			err := cur.Decode(&elem)
-			checkErr(err)
-			contacts = append(contacts, &elem)
+		json.NewEncoder(w).Encode(deleteResult)
+	case "GET":
+		if len(param) == 0 {
+			filter := bson.D{{}}
+			var contacts []*Contact
+
+			cur, err := collection.Find(context.TODO(), filter)
+			if err != nil {
+				// TODO: Send appropriate HTTP codes
+				sendErr(w, err, "Couldn't fetch the records. Try again.")
+				return
+			}
+			defer cur.Close(context.TODO())
+			for cur.Next(context.TODO()) {
+				var contact Contact
+				err := cur.Decode(&contact)
+				checkErr(err)
+				contacts = append(contacts, &contact)
+			}
+
+			if err := cur.Err(); err != nil {
+				sendErr(w, err, "Couldn't fetch the records. Try again.")
+				return
+			}
+
+			json.NewEncoder(w).Encode(contacts)
+		} else {
+			id, _ := primitive.ObjectIDFromHex(param)
+			filter := bson.M{"_id": id}
+
+			var contact Contact
+			err := collection.FindOne(context.TODO(), filter).Decode(&contact)
+			if err != nil {
+				if err.Error() == "mongo: no documents in result" {
+					sendErr(w, err, "No records match the given parameter.")
+				} else {
+					sendErr(w, err, "Couldn't fetch the records. Retry.")
+				}
+				return
+			}
+			json.NewEncoder(w).Encode(contact)
 		}
 
-		if err := cur.Err(); err != nil {
-			sendErr(w, err, "Couldn't fetch the records. Try again.")
-			return
-		}
-
-		json.NewEncoder(w).Encode(contacts)
 	default:
 		fmt.Println("Illegal Method")
 
@@ -119,12 +152,12 @@ func getClient() *mongo.Client {
 
 func main() {
 	config.SetVariable()
-	fmt.Printf("Connecting to %v ...\n", os.Getenv("MONGODB_URI"))
+	fmt.Printf("Connecting to %v\n", os.Getenv("MONGODB_URI"))
 	client = getClient()
 
 	httpPort := os.Getenv("PORT")
 	portString := fmt.Sprintf(":%s", httpPort)
-	http.HandleFunc("/api", apiHandler)
+	http.HandleFunc("/api/", apiHandler)
 
 	fmt.Printf("Server starting on http://localhost:%s\n", httpPort)
 
